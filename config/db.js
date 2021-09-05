6b72b4010063fffdb93362f6385e198609e4ac5d
mayefeng <mayefeng@163.com>
2021-09-05 14:19:37 +0800

    innit

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** MONGODB **/
+const mongodbUrl = 'mongodb://mayefeng:123456@localhost:27017/admin'; 
+const mongodbDbName = 'imooc-cli'
+
+module.exports = {
+    mongodbUrl,
+    mongodbDbName
+}
