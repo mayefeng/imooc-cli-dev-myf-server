13d3e167f1010229119dad64f752a5746dad7028
mayefeng <mayefeng@163.com>
2021-10-05 16:53:19 +0800

    WebSocket客户端和服务端通信优化

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -3,10 +3,19 @@
 module.exports = () => {
   return async (ctx, next) => {
     console.log('connect');
-    const { socket, logger } = ctx;
+    const { socket, logger, helper } = ctx;
+    const { id } = socket;
     const query = socket.handshake.query;
-    logger.info('query', query);
-    await next();
-    console.log('disconnect!');
+    try {
+      logger.info('query', query);
+      socket.emit(id, helper.parseMsg('connect', {
+        type: 'connect',
+        message: '云构建服务连接成功',
+      }));
+      await next();
+      console.log('disconnect!');
+    } catch (e) {
+      logger.error('build error', e);
+    }
   };
 };
